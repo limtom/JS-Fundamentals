60c62ca9ee62e0b030380ebd98f0713e7ada44e5
limtom <horlartom@live.com>
2025-11-07 13:16:22 +0100

    Excercise-7

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,22 @@
+const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+//Using for-loop
+for (let index = 0; index < myArray.length; index++) {
+  console.log(myArray[index]);
+}
+
+/* Using the while loop 
+let index = 0;
+while (myArray.length > index) {
+  console.log(myArray[index]);
+  index++;
+}
+*/
+
+/* Using the Do - while loop 
+let index = 0;
+do {
+  console.log(myArray[index]);
+  index++;
+} while (index < myArray.length);
+*/
