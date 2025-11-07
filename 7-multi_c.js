5bebd45c3d42ffcad12268834528c0d59ef2e3c8
limtom <horlartom@live.com>
2025-11-07 13:38:10 +0100

    Excercise-8 added

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const { argv } = require("node:process");
+
+const args = argv.slice(2);
+// const validArgs = parseInt(args[0]);
+if (parseInt(args[0])) {
+  for (var i = 0; i < args[0]; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
