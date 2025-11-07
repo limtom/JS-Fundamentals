d897823146dcc816b8ebc481964b1d9bbd197254
limtom <horlartom@live.com>
2025-11-07 11:54:31 +0100

    Bug fixed

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,9 @@
 const { argv } = require("node:process");
 
-argv.forEach((val, index) => {
-  if (argv.length === 2) {
-    console.log("No argument");
-  } else if (argv.length > 2 && argv.length < 4) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
-});
+if (argv.length === 2) {
+  console.log("No argument");
+} else if (argv.length > 2 && argv.length < 4) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
