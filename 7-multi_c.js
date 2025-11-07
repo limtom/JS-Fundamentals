b257aa5e7ba5f5c5aed2f51195240af948b44321
limtom <horlartom@live.com>
2025-11-07 13:43:00 +0100

    Another bug fixed on excercise-8

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,7 +3,7 @@ const { argv } = require("node:process");
 const args = argv.slice(2);
 // const validArgs = parseInt(args[0]);
 if (parseInt(args[0])) {
-  for (const i = 0; i < args[0]; i++) {
+  for (let i = 0; i < args[0]; i++) {
     console.log("C is fun");
   }
 } else {
