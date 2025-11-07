f453194dab55c678fd4638e8b80330696331db5f
limtom <horlartom@live.com>
2025-11-07 15:39:06 +0100

    Excercise-9

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const args = process.argv[2];
+
+console.log(args);
+if (parseInt(args)) {
+  for (let i = 0; i < args; i++) {
+    let row = "";
+    for (let j = 0; j < args; j++) {
+      row += "X";
+    }
+    console.log(row);
+  }
+} else {
+  console.log("Missing size");
+}
